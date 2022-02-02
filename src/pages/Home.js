c30dc1d76e142f750ee744862d9310f684b8ec17
Zeverai <zeverai.contact@gmail.com>
2022-02-02 15:09:42 -0500

    EOD commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 // ________________________________________________________________| Imports
 import React, { Fragment } from "react";
-
+import image from "../images/IMG-8556.jpg";
 // ________________________________________________________________| Home
 const Home = () => {
    return (
       <div className='home-container'>
          <div className='home-meat-wrapper'>
-            <div className='home-image-wrapper'>
-               <img className='home-image' alt='Portfolio Header Picture' />
-            </div>
+            <div className='home-image-wrapper'></div>
          </div>
       </div>
    );
