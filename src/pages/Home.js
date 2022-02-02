eabc7c1a1abc86ceead8b8f951a5900c7712dda7
Zeverai <zeverai.contact@gmail.com>
2022-02-02 14:18:20 -0500

    CHANGED animation timing on :hover

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,15 @@ import React, { Fragment } from "react";
 
 // ________________________________________________________________| Home
 const Home = () => {
-   return <div className='home-container'></div>;
+   return (
+      <div className='home-container'>
+         <div className='home-meat-wrapper'>
+            <div className='home-image-wrapper'>
+               <img className='home-image' alt='Portfolio Header Picture' />
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Home;
